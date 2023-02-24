2d3658194b7feb005e7affa0d0f446c8057669d2
NeerajMalviya1997 <nmalviya336@gmail.com>
2023-02-24 13:24:24 +0530

    first commit

---
+++ src/main/java/com/BottomFunnel/TravelFlap/TravelFlapApplication.java
@@ -0,0 +1,16 @@
+package com.BottomFunnel.TravelFlap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelFlapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelFlapApplication.class, args);
+		System.out.println("welcome to remote work");
+	}
+
+	
+	
+}
